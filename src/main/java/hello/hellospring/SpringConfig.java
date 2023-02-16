e2c67b259bc7cd7acf4f7b317203e1d2f2d41098
SangEun123 <101400455+SangEun123@users.noreply.github.com>
2023-02-16 20:39:57 +0900

    [feature/member] ADD: 데이터베이스 연동 및 설정 + 통합테스트(입문_섹션6) (#6)
    * [feature/setting] ADD: 프로젝트 환경설정 완료(입문_섹션1)
    
    * [feature/setting] ADD: 스프링 웹 기초 예제+회원 도메인과 리포지토리 만들기(입문_섹션2+섹션3:일부))
    
    * [feature/member] ADD: 회원 리포지토리, 서비스(섹션3:일부)
    
    * [feature/member] ADD: 회원 리포지토리, 서비스, 컨트롤러에 의존관계 설정(입문_섹션4)
    
    * [feature/member] ADD: 데이터베이스 연동 및 설정 + 통합테스트(입문_섹션6)
    
    ---------
    
    Co-authored-by: SangEunPark23 <101485039+SangEunPark23@users.noreply.github.com>

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,11 +1,20 @@
 package hello.hellospring;
 
+
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 
 /*
 * ***자동 의존 관계 설정 방식***
@@ -13,13 +22,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
     }
 
     @Bean
-    public MemberRepository memberRepository(){
+    public MemberRepository memberRepository
+
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
         return new MemoryMemberRepository();
     }
 
