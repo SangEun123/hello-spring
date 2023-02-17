60fb1f32180c23d210a644c1247e9eda2232a64c
SangEun123 <101400455+SangEun123@users.noreply.github.com>
2023-02-17 12:30:28 +0900

    [feature/member] ADD: JPA, 스프링데이터 JPA 라이브러리 추가 및 구현(입문_섹션6) (#8)
    * [feature/setting] ADD: 프로젝트 환경설정 완료(입문_섹션1)
    
    * [feature/setting] ADD: 스프링 웹 기초 예제+회원 도메인과 리포지토리 만들기(입문_섹션2+섹션3:일부))
    
    * [feature/member] ADD: 회원 리포지토리, 서비스(섹션3:일부)
    
    * [feature/member] ADD: 회원 리포지토리, 서비스, 컨트롤러에 의존관계 설정(입문_섹션4)
    
    * [feature/member] ADD: 데이터베이스 연동 및 설정 + 통합테스트(입문_섹션6)
    
    * [feature/member] ADD: JdbcTemplate 사용 + MemberService&SpringConfig 코드 에러 Refactoring(입문_섹션6)
    
    * [feature/member] ADD: JPA, 스프링데이터 JPA 라이브러리 추가 및 구현(입문_섹션6)
    
    ---------
    
    Co-authored-by: SangEunPark23 <101485039+SangEunPark23@users.noreply.github.com>

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,19 +1,12 @@
 package hello.hellospring;
 
 
-import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import hello.hellospring.service.MemberService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /*
  * ***자동 의존 관계 설정 방식***
@@ -21,26 +14,41 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
+//    private DataSource dataSource;
+//
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
-    private DataSource dataSource;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+//    private EntityManager em;
+//
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+//        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
+//
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new MemoryMemberRepository();
+////        return new JdbcTemplateMemberRepository(dataSource);
+////        return new JpaMemberRepository(em);
+//    }
 
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new MemoryMemberRepository();
-        return new JdbcTemplateMemberRepository(dataSource);
-    }
 
 }
--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,17 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 //회원 객체
+@Entity
 public class Member {
-    //시스템 저장용 아이디
+
+//    IDENTITY: DB에서 아이디를 자동 생성해주는 것
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //시스템 저장용 아이디; PK
     private Long id;
     private String name;
 
